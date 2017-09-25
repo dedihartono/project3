437f4292aaeb9fcaba4795f33c15213765f42c1c
Dedi Hartono <dedihartono1993@gmail.com>
2017-09-25 22:08:26 +0700

    templating

---
+++ application/views/navigasi/nav.php
@@ -0,0 +1,28 @@
+<!-- menu prile quick info -->
+<div class="profile">
+    <div class="profile_pic">
+        <img src="<?php echo base_url();?>assets/images/img.jpg" alt="..." class="img-circle profile_img">
+    </div>
+    <div class="profile_info">
+        <span>Welcome,</span>
+        <h2>Anthony Fernando</h2>
+    </div>
+</div>
+<!-- /menu prile quick info -->
+<br />
+<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+    <div class="menu_section">
+        <h3>General</h3>
+        <br>
+
+        <ul class="nav side-menu">
+            <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu" style="display: none">
+                    <li><a href="index.html">Dashboard</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+
+</div>
---
+++ application/views/template_admin.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="<?php echo base_url(); ?>assets/images/favicon.ico">
+
+    <title>Gentallela Alela! | </title>
+
+    <!-- Bootstrap core CSS -->
+
+    <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="<?php echo base_url();?>assets/fonts/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/css/animate.min.css" rel="stylesheet">
+
+    <!-- Custom styling plus plugins -->
+    <link href="<?php echo base_url();?>assets/css/custom.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/maps/jquery-jvectormap-2.0.1.css" />
+    <link href="<?php echo base_url();?>assets/css/icheck/flat/green.css" rel="stylesheet" />
+    <link href="<?php echo base_url();?>assets/css/floatexamples.css" rel="stylesheet" type="text/css" />
+
+
+    <script src="<?php echo base_url();?>assets/js/jquery.min.js"></script>
+
+    <!--[if lt IE 9]>
+        <script src="../assets/js/ie8-responsive-file-warning.js"></script>
+        <![endif]-->
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+</head>
+
+
+<body class="nav-md">
+
+    <div class="container body">
+
+
+        <div class="main_container">
+
+            <div class="col-md-3 left_col">
+                <div class="left_col scroll-view">
+
+                    <div class="navbar nav_title" style="border: 0;">
+                        <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentellela Alela!</span></a>
+                    </div>
+                    <div class="clearfix"></div>
+
+
+
+
+                    <!-- sidebar menu -->
+                    <?php $this->load->view('navigasi/nav') ;?>
+                    <!-- /sidebar menu -->
+
+                </div>
+            </div>
+
+            <!-- top navigation -->
+            <div class="top_nav">
+
+                <div class="nav_menu">
+                    <nav class="" role="navigation">
+                        <div class="nav toggle">
+                            <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+                        </div>
+
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="">
+                                <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                                    <img src="<?php echo base_url();?>assets/images/img.jpg" alt="">John Doe
+                                    <span class=" fa fa-angle-down"></span>
+                                </a>
+                                <ul class="dropdown-menu dropdown-usermenu animated fadeInDown pull-right">
+                                    <li><a href="javascript:;">  Profile</a></li>
+                                    <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
+                                    </li>
+                                </ul>
+                            </li>
+
+
+
+                        </ul>
+                    </nav>
+                </div>
+
+            </div>
+            <!-- /top navigation -->
+
+
+            <!-- page content -->
+            <div class="right_col" role="main">
+                  <div class="row">
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+
+                        </div>
+                  </div>
+
+
+                <!-- footer content -->
+                <footer>
+                    <div class="">
+                        <p class="pull-right">Gentelella Alela! a Bootstrap 3 template by <a>Kimlabs</a>. |
+                            <span class="lead"> <i class="fa fa-paw"></i> Gentelella Alela!</span>
+                        </p>
+                    </div>
+                    <div class="clearfix"></div>
+                </footer>
+                <!-- /footer content -->
+
+            </div>
+            <!-- /page content -->
+        </div>
+      </div>
+
+
+
+    <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+
+    <!-- chart js -->
+    <script src="<?php echo base_url();?>assets/js/chartjs/chart.min.js"></script>
+    <!-- bootstrap progress js -->
+    <script src="<?php echo base_url();?>assets/js/progressbar/bootstrap-progressbar.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/nicescroll/jquery.nicescroll.min.js"></script>
+    <!-- icheck -->
+    <script src="<?php echo base_url();?>assets/js/icheck/icheck.min.js"></script>
+
+    <!-- daterangepicker -->
+    <script type="text/javascript" src="<?php echo base_url();?>assets/js/moment.min2.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>assets/js/datepicker/daterangepicker.js"></script>
+    <!-- sparkline -->
+    <script src="<?php echo base_url();?>assets/js/sparkline/jquery.sparkline.min.js"></script>
+
+    <script src="<?php echo base_url();?>assets/js/custom.js"></script>
+    <!-- skycons -->
+    <script src="<?php echo base_url();?>assets/js/skycons/skycons.js"></script>
+
+    <!-- flot js -->
+    <!--[if lte IE 8]><script type="text/javascript" src="<?php echo base_url();?>assets/js/excanvas.min.js"></script><![endif]-->
+    <script type="text/javascript" src="<?php echo base_url();?>assets/js/flot/jquery.flot.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>assets/js/flot/jquery.flot.pie.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>assets/js/flot/jquery.flot.orderBars.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>assets/js/flot/jquery.flot.time.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>assets/js/flot/date.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>assets/js/flot/jquery.flot.spline.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>assets/js/flot/jquery.flot.stack.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>assets/js/flot/curvedLines.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>assets/js/flot/jquery.flot.resize.js"></script>
+
+</body>
+
+</html>
